8928f3150fccc37c7c5652240c4ee847958f209c
ikepeng <pippang@163.com>
2021-12-11 11:02:02 +0800

    first commit

---
+++ bussiness/src/main/java/org/example/bussiness/Hello.java
@@ -0,0 +1,15 @@
+package org.example.bussiness;
+
+import org.example.common.Table;
+
+
+/**
+ * @author Yali
+ * @date 2021-12-11 10:57:30
+ * @description
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        new Table();
+    }
+}
---
+++ common/src/main/java/org/example/common/Table.java
@@ -0,0 +1,19 @@
+package org.example.common;
+
+/**
+ * @author Yali
+ * @date 2021-12-11 10:55:57
+ * @description
+ **/
+public class Table {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
